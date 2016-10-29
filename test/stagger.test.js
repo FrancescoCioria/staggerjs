0c12a8795bfcff9d57533ec5faee7ec7232c125b
Francesco <firefelix@gmail.com>
2016-10-29 18:04:58 +0200

    add test for "maxOngoingMethods"

--- test/stagger.test.js
+++ test/stagger.test.js
@@ -20,4 +20,23 @@ describe('staggerjs', () => {
     return true;
   });
 
+  fit('maxOngoingMethods', async () => {
+    const timeout = 1000;
+    const fakeMethod = () => new Promise(resolve => setTimeout(() => resolve(Date.now()), timeout));
+    const methods = [...Array(10).keys()].map(() => fakeMethod);
+    const maxOngoingMethods = 5;
+
+    const res = await stagger(methods, { maxOngoingMethods, perSecond: Infinity });
+
+    const diff1 = res.slice(0, maxOngoingMethods).map((x, i) => i > 0 ? x - res[i - 1] : 0);
+    const diff2 = res.slice(maxOngoingMethods, res.length).map((x, i) => i > 0 ? x - res[maxOngoingMethods + i - 1] : 0);
+
+    const failed = diff1.concat(diff2).filter(d => d > 5);
+    if (failed.length > 0) {
+      throw new Error(`FAILED ${failed.length} times`);
+    }
+
+    return true;
+  });
+
 });
