dd0dd91fb5e2eec61611e856b4099d487a143625
Francesco <firefelix@gmail.com>
2016-10-29 17:36:02 +0200

    add tests for "perSecond" setting

---
+++ test/stagger.test.js
@@ -0,0 +1,23 @@
+import stagger from '../src';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000; // eslint-disable-line no-undef
+
+describe('staggerjs', () => {
+
+  it('perSecond', async () => {
+    const fakeMethod = () => Promise.resolve(Date.now());
+    const methods = [...Array(10).keys()].map(() => fakeMethod);
+    const perSecond = 1;
+
+    const res = await stagger(methods, { perSecond, maxOngoingMethods: 1 });
+    const diff = res.slice(1, res.length).map((x, i) => x - res[i]);
+
+    const failed = diff.filter(d => Math.abs((1000 / perSecond) - d) > 10);
+    if (failed.length > 0) {
+      throw new Error(`FAILED ${failed.length} times`);
+    }
+
+    return true;
+  });
+
+});
