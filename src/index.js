769db36ac2a9fb2b0cd5b5e2a40e87ab494d190a
Francesco <firefelix@gmail.com>
2016-10-29 17:36:53 +0200

    refactor code with "throttle-function" lib and make tests pass!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import t from 'tcomb';
+import _throttle from 'throttle-function';
 
 const Methods = t.list(t.Function);
 const Settings = t.interface({
@@ -17,43 +18,25 @@ export default (_methods, _settings) => {
 
   const { maxOngoingMethods, perSecond } = settings;
 
+  const throttle = _throttle(method => method(), 1000 / perSecond);
+
   return new Promise((resolve) => {
-    const minTimeBetweenMethods = 1000 / perSecond;
+    const queue = [];
 
-    let done = [];
-    let ongoing = [];
-    let stack = [];
-    let lastRun = null;
+    const onDone = res => {
+      throttle(() => runMethod(methods[queue.length]));
 
-    const runMethod = method => {
-      lastRun = Date.now();
-      const id = `${Math.random()}`;
-      const onDone = res => {
-        // move to "done"
-        ongoing = ongoing.filter(x => x.id !== id);
-        done = done.concat(res);
-
-        if (stack.length > 0) {
-          // run next method
-          const timeoutMillis = Math.max(minTimeBetweenMethods - (Date.now() - lastRun), 0);
-
-          const run = () => {
-            runMethod(stack[0]);
-            // update stack
-            stack = stack.slice(1, stack.length);
-          };
-
-          timeoutMillis > 0 && setTimeout(run, timeoutMillis);
-          timeoutMillis <= 0 && run();
-        } else {
-          resolve(done);
-        }
-      };
-
-      ongoing = ongoing.concat({ id, promise: method().then(onDone).catch(onDone) });
+      return res;
     };
 
-    stack = stack.concat(methods.slice(maxOngoingMethods, stack.length - 1));
-    ongoing = ongoing.concat(methods.slice(0, maxOngoingMethods).map((m, i) => setTimeout(() => runMethod(m), minTimeBetweenMethods * i)));
+    const runMethod = method => {
+      if (queue.length !== methods.length) {
+        queue.push(method().then(onDone).catch(onDone));
+      } else {
+        resolve(Promise.all(queue));
+      }
+    };
+
+    methods.slice(0, maxOngoingMethods).forEach((m, i) => i === 0 ? runMethod(m) : throttle(() => runMethod(m)));
   });
 };
